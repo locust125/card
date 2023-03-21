e0ee022aa20e07bb79ed2691cc968701c46e5fa5
locust125 <lalom1227@gmail.com>
2023-03-20 22:23:50 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:card/widgets/formulario.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Pago con tarjeta'),
         ),
-        body: const formulario(),
+        body: const (),
       ),
     );
   }
